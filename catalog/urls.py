de524feb4bbac964a65c6e1583084e53ed3d6755
kylejohnson1 <kyle.johnson1@outlook.com>
2020-05-31 17:15:22 -0700

    Finished part 8 of tutorial.

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'), # uses a class-based view
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
 ]
 
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# Redirect any emails to the console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
